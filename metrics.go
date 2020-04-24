ad3dd21928a9935a84f34f6a3e78ce80145b0186
Rebecca Bilbro <bilbro@gmail.com>
2020-04-24 08:42:32 -0400

    define prometheus metrics, still need to add them into the code

---
+++ metrics.go
@@ -0,0 +1,109 @@
+package radish
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/kansaslabs/x/out"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	pmWorkers        prometheus.Gauge         // number of available workers
+	pmQueueSize      prometheus.Gauge         // number of tasks in the queue awaiting handling
+	pmPercentFull    prometheus.Gauge         // the percent of the queue that is full * 100
+	pmPercentSuccess *prometheus.GaugeVec     // the percent of tasks successfully completed, labeled by task
+	pmTasksSucceeded *prometheus.CounterVec   // the count of successfully completed tasks, labeled by task type
+	pmTasksFailed    *prometheus.CounterVec   // the count of failed tasks, labeled by task type
+	pmTaskLatency    *prometheus.HistogramVec // the time it is taking for tasks to complete, labeled by task type, success, and failure
+)
+
+const (
+	pmNamespace = "radish"
+	pmSubsystem = "queue"
+)
+
+func initMetrics() {
+	pmWorkers = prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: pmNamespace,
+		Subsystem: pmSubsystem,
+		Name:      "workers",
+		Help:      "The number of available workers",
+	})
+
+	pmQueueSize = prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: pmNamespace,
+		Subsystem: pmSubsystem,
+		Name:      "queue_size",
+		Help:      "number of tasks in the queue awaiting handling",
+	})
+
+	pmPercentFull = prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: pmNamespace,
+		Subsystem: pmSubsystem,
+		Name:      "percent_full",
+		Help:      "the percent of the queue that is already full",
+	})
+
+	pmPercentSuccess = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: pmNamespace,
+		Subsystem: pmSubsystem,
+		Name:      "percent_success",
+		Help:      "the percent of tasks successfully completed, labeled by task",
+	}, []string{"task"})
+
+	pmTasksSucceeded = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace: pmNamespace,
+		Subsystem: pmSubsystem,
+		Name:      "tasks_succeeded",
+		Help:      "the count of tasks successfully completed, labeled by task type",
+	}, []string{"task"})
+
+	pmTasksFailed = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace: pmNamespace,
+		Subsystem: pmSubsystem,
+		Name:      "tasks_failed",
+		Help:      "the count of failed tasks, labeled by task type",
+	}, []string{"task"})
+
+	pmTaskLatency = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: pmNamespace,
+		Subsystem: pmSubsystem,
+		Name:      "task_latency",
+		Help:      "time to task completion, labeled by task type, success, and failure",
+	}, []string{"task", "result"})
+}
+
+func serveMetrics(metricsAddr string) {
+	out.Status("serving prometheus metrics at http://%s/metrics", metricsAddr)
+	http.Handle("/metrics", promhttp.Handler())
+	if err := http.ListenAndServe(metricsAddr, nil); err != nil {
+		out.Warne(err)
+	}
+}
+
+func registerMetrics() error {
+	if err := prometheus.Register(pmWorkers); err != nil {
+		return fmt.Errorf("did not register %s", pmWorkers, err)
+	}
+	if err := prometheus.Register(pmQueueSize); err != nil {
+		return fmt.Errorf("did not register %s", pmQueueSize, err)
+	}
+	if err := prometheus.Register(pmPercentFull); err != nil {
+		return fmt.Errorf("did not register %s", pmPercentFull, err)
+	}
+	if err := prometheus.Register(pmPercentSuccess); err != nil {
+		return fmt.Errorf("did not register %s", pmPercentSuccess, err)
+	}
+	if err := prometheus.Register(pmTasksSucceeded); err != nil {
+		return fmt.Errorf("did not register %s", pmTasksSucceeded, err)
+	}
+	if err := prometheus.Register(pmTasksFailed); err != nil {
+		return fmt.Errorf("did not register %s", pmTasksFailed, err)
+	}
+	if err := prometheus.Register(pmTaskLatency); err != nil {
+		return fmt.Errorf("did not register %s", pmTaskLatency, err)
+	}
+}
