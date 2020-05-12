0d535ef75e8d33e8bd38b50361b29bab8ddc8034
Rebecca Bilbro <rebeccabilbro@users.noreply.github.com>
2020-05-12 07:12:27 -0400

    Prometheus metrics (#11)
    Adds 6 new metrics to track radish task and worker progress.
    
    Closes #5

--- worker.go
+++ worker.go
@@ -1,6 +1,10 @@
 package radish
 
-import "github.com/kansaslabs/x/out"
+import (
+	"time"
+
+	"github.com/kansaslabs/x/out"
+)
 
 type worker struct {
 	parent *Radish   // the parent of the worker that has the tasks queue and the handlers
@@ -14,6 +18,13 @@ taskloop:
 		case <-w.stop:
 			return
 		case task := <-w.parent.tasks:
+
+			// Update the queue size and percent full
+			pmQueueSize.Set(float64(len(w.parent.tasks)))
+			pmPercentFull.Set(float64(len(w.parent.tasks)) / float64(w.parent.config.QueueSize) * 100)
+
+			start := time.Now()
+
 			handler, err := w.parent.Handler(task.Task)
 			if err != nil {
 				// Unregistered task
@@ -26,11 +37,26 @@ taskloop:
 				// Task failure
 				out.Caution(err.Error())
 				handler.Failure(task.ID, err, task.Failure)
+
+				// Compute latency in milliseconds
+				latency := float64(time.Since(start)/1000) / 1000.0
+				pmTaskLatency.WithLabelValues(task.Task, "failed").Observe(latency)
+
+				// Update prometheus metrics with failed task
+				pmTasksFailed.WithLabelValues(task.Task).Inc()
 			} else {
 				// Task success
 				out.Debug("finished %s task %s", task.Task, task.ID)
 				handler.Success(task.ID, task.Success)
+
+				// Compute latency in milliseconds
+				latency := float64(time.Since(start)/1000) / 1000.0
+				pmTaskLatency.WithLabelValues(task.Task, "succeeded").Observe(latency)
+
+				// Update prometheus metrics with succeeded task
+				pmTasksSucceeded.WithLabelValues(task.Task).Inc()
 			}
+
 		}
 	}
 }
