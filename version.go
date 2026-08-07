88ef5d5accde6db28ca2a3c47f84254ad8d8e05d
Benjamin Bengfort <benjamin@bengfort.com>
2026-08-07 08:24:56 -0500

    version bump v1.2.2-beta.5

--- version.go
+++ version.go
@@ -14,9 +14,9 @@ import (
 const (
 	VersionMajor         = 1
 	VersionMinor         = 2
-	VersionPatch         = 1
+	VersionPatch         = 2
 	VersionReleaseLevel  = "beta"
-	VersionReleaseNumber = 4
+	VersionReleaseNumber = 5
 )
 
 // Set the GitVersion via -ldflags="-X 'go.rtnl.ai/endeavor/pkg.GitVersion=$(git rev-parse --short HEAD)'"
